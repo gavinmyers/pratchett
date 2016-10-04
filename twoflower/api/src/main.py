b162fa1a934233b7cb659b00ff7694d48a74b641
gavin myers <gavin@gavinm.com>
2016-10-03 21:08:40 -0500

    getting compose to work

--- twoflower/api/src/main.py
+++ twoflower/api/src/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
-import requests 
+import requests, redis 
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  r = redis.StrictRedis(host='redis', port=6379, db=0)
+  r.set('foo', 'bar')
+  return r.get('foo') 
 
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0',port=5000)
